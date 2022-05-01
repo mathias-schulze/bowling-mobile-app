4cf156f722df09dde46464ce3b5ceb8a43fa457f
Mathias Schulze <mathias-schulze@web.de>
2022-05-01 23:08:19 +0200

    basic UI structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +8,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bowling',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DefaultTabController(
+        length: 3,
+        child: MyHomePage(title: 'Bowling'),
+      )
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +33,162 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.format_list_numbered)),
+              Tab(icon: Icon(Icons.calendar_month)),
+              Tab(icon: Icon(Icons.people)),
+            ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: const TabBarView(
+          children: [
+            ResultTab(),
+            DateTab(),
+            PlayerTab(),
+          ]
+      ),
     );
   }
+}
+
+class ResultTab extends StatefulWidget {
+  const ResultTab({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ResultTabState();
+}
+
+class _ResultTabState extends State<ResultTab> {
+
+  final NumberFormat numberFormat = NumberFormat("###.0#", "de_De");
+
+  final _results = <_Result>[
+    _Result("Mathias", 200.0),
+    _Result("Norbert", 150.0),
+    _Result("Fahd", 145.0),
+    _Result("Christian", 140.0),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: _results.length,
+          itemBuilder: (context, index) {
+            var result = _results[index];
+            return ListTile(
+              title: Text(result._player + " " + numberFormat.format(result._score)),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => () {},
+        tooltip: 'Spieler hinzufügen',
+        child: const Icon(Icons.group_add),
+      ),
+    );
+  }
+}
+
+class _Result {
+  final String _player;
+  final double _score;
+
+  _Result(this._player, this._score);
+}
+
+class DateTab extends StatefulWidget {
+  const DateTab({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _DateTabState();
+}
+
+class _DateTabState extends State<DateTab> {
+
+  final DateFormat dateFormat = DateFormat("dd.MM.yyyy");
+
+  final _dates = <_Date>[
+    _Date(DateTime.parse("2020-02-04"), "XXL Bowling 117"),
+    _Date(DateTime.parse("2020-03-03"), "XXL Bowling 118"),
+    _Date(DateTime.parse("2022-05-03"), "XXL Bowling 119"),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: _dates.length,
+          itemBuilder: (context, index) {
+            var date = _dates[index];
+            return ListTile(
+              title: Text(dateFormat.format(date._date) + " " + date._desc),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => () {},
+        tooltip: 'Termin anlegen',
+        child: const Icon(Icons.insert_invitation),
+      ),
+    );
+  }
+}
+
+class _Date {
+  final DateTime _date;
+  final String _desc;
+
+  _Date(this._date, this._desc);
+}
+
+class PlayerTab extends StatefulWidget {
+  const PlayerTab({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _PlayerTabState();
+}
+
+class _PlayerTabState extends State<PlayerTab> {
+
+  final _player = <_Player>[
+    _Player("Christian"),
+    _Player("Fahd"),
+    _Player("Mathias"),
+    _Player("Norbert"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: _player.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_player[index]._name),
+          );
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => () {},
+        tooltip: 'Spieler anlegen',
+        child: const Icon(Icons.person_add),
+      ),
+    );
+  }
+}
+
+class _Player {
+  final String _name;
+
+  _Player(this._name);
 }
