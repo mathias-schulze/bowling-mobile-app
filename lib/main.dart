a4479b2d331529176269f8c055426c31e38f6d77
Mathias Schulze <mathias-schulze@web.de>
2022-05-08 21:29:00 +0200

    enable offline capabilities for Realtime Database

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseDatabase.instance.setPersistenceEnabled(true);
   runApp(const MyApp());
 }
 
